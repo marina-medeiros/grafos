b326c1f7dcc7c34e4a5d750ad33d0c838e940371
marina-medeiros <marinamaleite@gmail.com>
2025-11-12 19:51:44 -0300

    adicionando menu na main

--- unidade2/main.cpp
+++ unidade2/main.cpp
@@ -63,18 +63,7 @@ int main(){
 
     // ///////////// KRUSKAL //////////////////////////
     // GrafoMatrizAdj grafo0(5);
-    
-    // grafo0.inserir_aresta(1, 2, 1);
-    // grafo0.inserir_aresta(1, 3, 20);
-    // grafo0.inserir_aresta(2, 3, 17);
-    // grafo0.inserir_aresta(0, 2, 85);
-    // grafo0.inserir_aresta(0, 3, 22);
-    // grafo0.inserir_aresta(1, 4, 86);
-    // grafo0.inserir_aresta(3, 4, 23);
-    // grafo0.inserir_aresta(0, 4, 86);
-    // grafo0.inserir_aresta(0, 1, 15);
-    // grafo0.inserir_aresta(2, 4, 90);
-    
+
     // GrafoMatrizAdj AGM = gerar_agm_kruskal(grafo0);
 
     // analisar_e_gerar_imagem(grafo0, "grafo_0", "matriz_adj");
@@ -84,50 +73,40 @@ int main(){
 
     // ///////////// DIJKSTRA //////////////////////////
     // GrafoMatrizAdj grafo1(7);
-    // grafo1.inserir_aresta(0, 1, 2);
-    // grafo1.inserir_aresta(0, 3, 5);
-    // grafo1.inserir_aresta(0, 2, 4);
-    // grafo1.inserir_aresta(1, 3, 2);
-    // grafo1.inserir_aresta(2, 3, 1);
-    // grafo1.inserir_aresta(2, 5, 4);
-    // grafo1.inserir_aresta(3, 4, 2);
-    // grafo1.inserir_aresta(3, 5, 3);
-    // grafo1.inserir_aresta(4, 5, 1);
-    // grafo1.inserir_aresta(4, 6, 5);
-    // grafo1.inserir_aresta(5, 6, 7);
-
-
     // DigrafoMatrizAdj digrafo0(6);
-    // digrafo0.inserir_aresta(0, 1, 5);
-    // digrafo0.inserir_aresta(0, 2, 2);
-    // digrafo0.inserir_aresta(1, 2, 1);
-    // digrafo0.inserir_aresta(1, 3, 4);
-    // digrafo0.inserir_aresta(1, 4, 2);
-    // digrafo0.inserir_aresta(2, 4, 7);
-    // digrafo0.inserir_aresta(3, 4, 6);
-    // digrafo0.inserir_aresta(3, 5, 3);
-    // digrafo0.inserir_aresta(4, 5, 1);
-
     // dijkstra_geral(grafo1, 0);
     // dijkstra_geral(digrafo0, 0);
     // dijkstra_geral(grafo1, 6);
     // dijkstra_geral(digrafo0, 5);
     // dijkstra_geral(digrafo0, 2);
-
     // dijkstra_especifico(grafo1, 0, 4); // 6
     // dijkstra_especifico(grafo1, 6, 3); // 8
     // dijkstra_especifico(digrafo0, 2, 3); // 999
 }
 
 void agm_basico(){
-    // kruskal
+    GrafoMatrizAdj digrafo_AGM(0); // grafo grande que está no arquivo do trabalho
+    digrafo_AGM.carregar_de_arquivo("../dados/DIGRAFO_0.txt");
+
+    //------------------------------------ kruskal -----------------------------------------
+    // Exemplo dos slides
     GrafoMatrizAdj grafo_kruskal(0);
     grafo_kruskal.carregar_de_arquivo("../dados/GRAFO_KRUSKAL.txt");
-
     analisar_e_gerar_imagem(grafo_kruskal, "grafo_kruskal", "matriz_adj");
 
     GrafoMatrizAdj agm_kruskal = gerar_agm_kruskal(grafo_kruskal);
+    analisar_e_gerar_imagem(agm_kruskal, "agm_kruskal", "matriz_adj");
 
-    analisar_e_gerar_imagem(grafo_kruskal, "AGM_grafo0", "matriz_adj");
+    // Exemplo do pdf do trabalho
+    GrafoMatrizAdj agm_kruskal_digrafo =  gerar_agm_kruskal(digrafo_AGM);
+    analisar_e_gerar_imagem(agm_kruskal_digrafo, "agm_kruskal_digrafo", "matriz_adj");
 
+    //------------------------------------ prim -----------------------------------------
+
+
+    //------------------------------------ boruvka -----------------------------------------
+}
+
+void caminho_minimo(){
+    GrafoMatrizAdj grafo(0);
 }
