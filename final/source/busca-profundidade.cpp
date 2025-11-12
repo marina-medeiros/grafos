d66fab18c1084b18e3088e513e86234664969397
marina-medeiros <marinamaleite@gmail.com>
2025-11-12 11:44:38 -0300

    comentando prints no terminal

--- final/source/busca-profundidade.cpp
+++ final/source/busca-profundidade.cpp
@@ -94,21 +94,21 @@ std::pair<std::map<int, int>, std::vector<std::pair<int, int>>> busca_profundida
 
     std::cout << std::endl;
 
-    std::cout << std::endl;
-    std::cout << "---------------------\n";
-    std::cout << "Vértice | Predecessor \n";
-    std::cout << "---------------------\n";
-    for(const auto& par : predecessores) {
-        std::cout << "| " << rotulos[par.first] << "\t| "
-                << (par.second != -1 ? rotulos[par.second] : "Raiz") << "\t  | "
-                <<  std::endl;
-    }
-    std::cout << "---------------------\n";
+    // std::cout << std::endl;
+    // std::cout << "---------------------\n";
+    // std::cout << "Vértice | Predecessor \n";
+    // std::cout << "---------------------\n";
+    // for(const auto& par : predecessores) {
+    //     std::cout << "| " << rotulos[par.first] << "\t| "
+    //             << (par.second != -1 ? rotulos[par.second] : "Raiz") << "\t  | "
+    //             <<  std::endl;
+    // }
+    // std::cout << "---------------------\n";
 
-    std::cout << "\nArestas de Retorno Encontradas:" << std::endl;
-    for (const auto& aresta : arestas_retorno) {
-        std::cout << "(" << rotulos[aresta.first] << ", " << rotulos[aresta.second] << ")" << std::endl;
-    }
+    // std::cout << "\nArestas de Retorno Encontradas:" << std::endl;
+    // for (const auto& aresta : arestas_retorno) {
+    //     std::cout << "(" << rotulos[aresta.first] << ", " << rotulos[aresta.second] << ")" << std::endl;
+    // }
 
     return {predecessores, arestas_retorno};
 }
@@ -135,9 +135,7 @@ void busca_profundidade_lista_adj_recursiva_util(int u,
                                                 const std::vector<std::string>& rotulos) {
     visitados[u] = true;
     
-    std::cout << rotulos[u] << " ";
-
-    
+    //std::cout << rotulos[u] << " ";
 
     for (int v : lista_adj[u]) {
         if (!visitados[v]) {
