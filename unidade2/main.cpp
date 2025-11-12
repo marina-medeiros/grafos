6c17700f118822b0402988be6e2fd8bda5328be6
marina-medeiros <marinamaleite@gmail.com>
2025-11-12 11:54:52 -0300

    atualizando main

--- unidade2/main.cpp
+++ unidade2/main.cpp
@@ -10,19 +10,40 @@
 #include "headers/arvore-minima.h"
 #include "headers/kruskal.h"
 
+void analisar_e_gerar_imagem(Grafo& grafo, const std::string& nome_arquivo, const std::string& tipo_impl, bool eh_digrafo = false) {
+    std::cout << "\n\nAnálise do " << nome_arquivo << std::endl;
+
+    grafo.print();
+
+    // Testa os algoritmos
+    std::cout << "Total de Vertices: " << grafo.get_qtd_vertices() << std::endl;
+    std::cout << "Total de Arestas: " << grafo.get_qtd_arestas() << std::endl;
+        
+    // Gera a imagem de visualização   
+    std::string arq_dot =  nome_arquivo + "_" + tipo_impl + ".dot";
+    std::string arq_png = nome_arquivo + "_" + tipo_impl + ".png";
+    
+    std::cout << "Gerando imagem em '" << arq_png << "'...\n";
+    grafo.exportar_para_dot(arq_dot, eh_digrafo);
+    gerar_imagem(arq_dot, arq_png);
+}
+
 int main(){
-    GrafoMatrizAdj grafoMatrizAdj(5);
+    GrafoMatrizAdj grafo0(5);
     
-    grafoMatrizAdj.inserir_aresta(1, 2, 1);
-    grafoMatrizAdj.inserir_aresta(1, 3, 20);
-    grafoMatrizAdj.inserir_aresta(2, 3, 17);
-    grafoMatrizAdj.inserir_aresta(0, 2, 85);
-    grafoMatrizAdj.inserir_aresta(0, 3, 22);
-    grafoMatrizAdj.inserir_aresta(1, 4, 86);
-    grafoMatrizAdj.inserir_aresta(3, 4, 23);
-    grafoMatrizAdj.inserir_aresta(0, 4, 86);
-    grafoMatrizAdj.inserir_aresta(0, 1, 15);
-    grafoMatrizAdj.inserir_aresta(2, 4, 90);
+    grafo0.inserir_aresta(1, 2, 1);
+    grafo0.inserir_aresta(1, 3, 20);
+    grafo0.inserir_aresta(2, 3, 17);
+    grafo0.inserir_aresta(0, 2, 85);
+    grafo0.inserir_aresta(0, 3, 22);
+    grafo0.inserir_aresta(1, 4, 86);
+    grafo0.inserir_aresta(3, 4, 23);
+    grafo0.inserir_aresta(0, 4, 86);
+    grafo0.inserir_aresta(0, 1, 15);
+    grafo0.inserir_aresta(2, 4, 90);
     
-    gerar_arvore_minima(grafoMatrizAdj);
+    GrafoMatrizAdj AGM = gerar_arvore_minima(grafo0);
+
+    analisar_e_gerar_imagem(grafo0, "grafo_0", "matriz_adj");
+    analisar_e_gerar_imagem(AGM, "AGM_grafo0", "matriz_adj");
 }
