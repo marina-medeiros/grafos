eef9fdf23eadbc3eb7bb1a886c0637222ddcd425
marina-medeiros <marinamaleite@gmail.com>
2025-11-12 15:57:43 -0300

    dijkstra geral e entre dois vertices específicos

--- unidade2/headers/dijkstra.h
+++ unidade2/headers/dijkstra.h
@@ -4,25 +4,12 @@
 #include <map>
 #include <set>
 #include <stack>
+#include <vector>
 #include <utility> 
 #include "Grafo.h"
 #include "GrafoListaAdj.h"
 
-std::map<int, int> busca_em_profundidade_grafo(const Grafo& grafo, int vertice_inicial);
-std::pair<std::map<int, int>, std::vector<std::pair<int, int>>> busca_profundidade_lista_adj_recursiva(GrafoListaAdj grafo, int vertice);
-void busca_profundidade_lista_adj_recursiva_util(int u,
-                                                std::vector<bool>& visitados,
-                                                std::map<int, int>& predecessores,
-                                                std::map<int, std::list<int>>& lista_adj,
-                                                std::vector<std::pair<int, int>>& arestas_retorno,
-                                                const std::vector<std::string>& rotulos);
-void exportar_arvore_profundidade_para_dot(const std::string& filename,
-                                            std::map<int, int> arvore, 
-                                            const std::vector<std::pair<int, int>>& arestas_retorno,
-                                            const std::vector<std::string>& rotulos);
-void busca_articulacoes_dfs_recursiva(
-    const Grafo& grafo,int u, std::vector<int>& disc, std::vector<int>& low, 
-    std::vector<int>& parent, std::stack<std::pair<int, int>>& pilha,
-    std::set<int>& ap, std::vector<std::set<int>>& blocos, int& tempo);
+std::vector<int> dijkstra_geral(const GrafoMatrizAdj& grafoMatrizAdj, int vertice_inicial);
+std::vector<int> dijkstra_especifico(const GrafoMatrizAdj& grafoMatrizAdj, int vertice_inicial, int vertice_final);
 
 #endif
--- unidade2/source/dijkstra.cpp
+++ unidade2/source/dijkstra.cpp
@@ -7,3 +7,167 @@
 #include "../headers/Grafo.h"
 #include "../headers/busca-profundidade.h"
 #include "../headers/GrafoListaAdj.h"
+#include "../headers/GrafoMatrizAdj.h"
+
+bool existem_nao_visitados(std::vector<int>& visitados){
+    for(int ii = 0; ii < int(visitados.size()); ii++){
+        if(visitados[ii] == 0){
+            return true;
+        }
+    }
+    return false;
+}
+
+int vertice_com_menor_distancia(std::vector<int>& distancia, std::vector<int>& visitado){
+    int indice_menor_distancia;
+    for(int ii = 0; ii < int(visitado.size()); ii++){
+        if(visitado[ii] == 0){
+            indice_menor_distancia = ii;
+            break;
+        }
+    }
+
+    for(int ii = 0; ii < int(distancia.size()); ii++){
+        if(visitado[ii] == 0){
+            if(distancia[ii] < distancia[indice_menor_distancia]){
+                indice_menor_distancia = ii;
+            }
+        }
+    }
+
+    return indice_menor_distancia;
+}
+
+std::vector<int> dijkstra_geral(const GrafoMatrizAdj& grafoMatrizAdj, int vertice_inicial){
+    int qtd_vertices = grafoMatrizAdj.get_qtd_vertices();
+    std::vector<std::vector<int>> matriz_adj = grafoMatrizAdj.get_matriz_adj();
+
+    std::vector<int> distancia(qtd_vertices, 999);
+    std::vector<int> predecessor(qtd_vertices, -1);
+    std::vector<int> visitado(qtd_vertices, 0);
+
+    visitado[vertice_inicial] = 1;
+    distancia[vertice_inicial] = 0;
+
+    for(int ii = 0; ii < qtd_vertices; ii++){
+        if(matriz_adj[vertice_inicial][ii] != 0){
+            predecessor[ii] = vertice_inicial;
+            distancia[ii] = matriz_adj[vertice_inicial][ii];
+        }
+    }
+
+    while(existem_nao_visitados(visitado)){
+        int vertice = vertice_com_menor_distancia(distancia, visitado);
+        visitado[vertice] = 1;
+        for(int ii = 0; ii < qtd_vertices; ii++){
+            if(matriz_adj[vertice][ii] != 0){
+                if(visitado[ii] == 0){
+                    if(distancia[ii] > (distancia[vertice] + matriz_adj[vertice][ii])){
+                        distancia[ii] = (distancia[vertice] + matriz_adj[vertice][ii]);
+                        predecessor[ii] = vertice;
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << "Dijkstra: menor distância do vértice " << vertice_inicial << " para os demais" << std::endl;
+    std::cout << "Vértices     | ";
+    for(int ii = 0; ii < int(visitado.size()); ii++){
+        if(ii < 10 && ii >= 0){ std::cout << " ";}
+        std::cout << ii << "  ";
+    }
+    std::cout << std::endl;
+    std::cout << "Visitado     | ";
+    for(int ii = 0; ii < int(visitado.size()); ii++){
+        if(visitado[ii] < 10 && visitado[ii] >= 0){ std::cout << " ";}
+        std::cout << visitado[ii] << "  ";
+    }
+    std::cout << std::endl;
+    std::cout << "Predecessor  | ";
+    for(int ii = 0; ii < int(predecessor.size()); ii++){
+        if(predecessor[ii] < 10 && predecessor[ii] >= 0){ std::cout << " ";}
+        std::cout << predecessor[ii] << "  ";
+    }
+    std::cout << std::endl;
+    std::cout << "Distância    | ";
+    for(int ii = 0; ii < int(distancia.size()); ii++){
+        if(distancia[ii] < 10 && distancia[ii] >= 0){ std::cout << " ";}
+        std::cout << distancia[ii] << "  ";
+    }
+    std::cout << std::endl << std::endl;
+
+    return distancia;
+}
+
+std::vector<int> dijkstra_especifico(const GrafoMatrizAdj& grafoMatrizAdj, int vertice_inicial, int vertice_final){
+    int qtd_vertices = grafoMatrizAdj.get_qtd_vertices();
+    std::vector<std::vector<int>> matriz_adj = grafoMatrizAdj.get_matriz_adj();
+
+    std::vector<int> distancia(qtd_vertices, 999);
+    std::vector<int> predecessor(qtd_vertices, -1);
+    std::vector<int> visitado(qtd_vertices, 0);
+
+    visitado[vertice_inicial] = 1;
+    distancia[vertice_inicial] = 0;
+
+    for(int ii = 0; ii < qtd_vertices; ii++){
+        if(matriz_adj[vertice_inicial][ii] != 0){
+            predecessor[ii] = vertice_inicial;
+            distancia[ii] = matriz_adj[vertice_inicial][ii];
+        }
+    }
+
+    while(visitado[vertice_final] == 0){
+        int vertice = vertice_com_menor_distancia(distancia, visitado);
+        visitado[vertice] = 1;
+        for(int ii = 0; ii < qtd_vertices; ii++){
+            if(matriz_adj[vertice][ii] != 0){
+                if(visitado[ii] == 0){
+                    if(distancia[ii] > (distancia[vertice] + matriz_adj[vertice][ii])){
+                        distancia[ii] = (distancia[vertice] + matriz_adj[vertice][ii]);
+                        predecessor[ii] = vertice;
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << "Dijkstra: menor distância do vértice " << vertice_inicial << " para " << vertice_final << std::endl;
+    std::cout << "Vértices     | ";
+    for(int ii = 0; ii < int(visitado.size()); ii++){
+        if(ii < 10 && ii >= 0){ std::cout << " ";}
+        if(ii < 100){ std::cout << " ";}
+        std::cout << ii << "  ";
+    }
+    std::cout << std::endl;
+    std::cout << "Visitado     | ";
+    for(int ii = 0; ii < int(visitado.size()); ii++){
+        if(visitado[ii] < 10 && visitado[ii] >= 0){ std::cout << " ";}
+        if(visitado[ii] < 100){ std::cout << " ";}
+        std::cout << visitado[ii] << "  ";
+    }
+    std::cout << std::endl;
+    std::cout << "Predecessor  | ";
+    for(int ii = 0; ii < int(predecessor.size()); ii++){
+        if(predecessor[ii] < 10 && predecessor[ii] >= 0){ std::cout << " ";}
+        if(predecessor[ii] < 100){ std::cout << " ";}
+        std::cout << predecessor[ii] << "  ";
+    }
+    std::cout << std::endl;
+    std::cout << "Distância    | ";
+    for(int ii = 0; ii < int(distancia.size()); ii++){
+        if(distancia[ii] < 10 && distancia[ii] >= 0){ std::cout << " ";}
+        if(distancia[ii] < 100){ std::cout << " ";}
+        std::cout << distancia[ii] << "  ";
+    }
+    std::cout << std::endl;
+
+    if(distancia[vertice_final] == 999){
+        std::cout << "Não há caminho entre os dois vértices." << std::endl;
+    }else{
+        std::cout << "Menor distância encontrada: " << distancia[vertice_final] << std::endl << std::endl;
+    }
+
+    return distancia;
+}
