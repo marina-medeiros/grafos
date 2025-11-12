a745d87399dafb52e74533bbda772cb60deac12e
marina-medeiros <marinamaleite@gmail.com>
2025-11-12 20:23:36 -0300

    removendo espaços dos aquivos da pasta dados

--- unidade2/main.cpp
+++ unidade2/main.cpp
@@ -65,19 +65,20 @@ int main(){
 void agm_basico(){
     GrafoMatrizAdj digrafo(0); // grafo grande que está no arquivo do trabalho
     digrafo.carregar_de_arquivo("../dados/DIGRAFO_0.txt");
+    analisar_e_gerar_imagem(digrafo, "digrafo", "matriz_adj");
 
     //------------------------------------ kruskal -----------------------------------------
     // Exemplo dos slides
-    GrafoMatrizAdj grafo_kruskal(0);
-    grafo_kruskal.carregar_de_arquivo("../dados/GRAFO_KRUSKAL.txt");
-    analisar_e_gerar_imagem(grafo_kruskal, "grafo_kruskal", "matriz_adj");
+    // GrafoMatrizAdj grafo_kruskal(0);
+    // grafo_kruskal.carregar_de_arquivo("../dados/GRAFO_KRUSKAL.txt");
+    // analisar_e_gerar_imagem(grafo_kruskal, "grafo_kruskal", "matriz_adj");
 
-    GrafoMatrizAdj agm_kruskal = gerar_agm_kruskal(grafo_kruskal);
-    analisar_e_gerar_imagem(agm_kruskal, "agm_kruskal", "matriz_adj");
+    // GrafoMatrizAdj agm_kruskal = gerar_agm_kruskal(grafo_kruskal);
+    // analisar_e_gerar_imagem(agm_kruskal, "agm_kruskal", "matriz_adj");
 
-    // Exemplo do pdf do trabalho
-    GrafoMatrizAdj agm_kruskal_digrafo =  gerar_agm_kruskal(digrafo);
-    analisar_e_gerar_imagem(agm_kruskal_digrafo, "agm_kruskal_digrafo", "matriz_adj");
+    // // Exemplo do pdf do trabalho
+    // GrafoMatrizAdj agm_kruskal_digrafo =  gerar_agm_kruskal(digrafo);
+    // analisar_e_gerar_imagem(agm_kruskal_digrafo, "agm_kruskal_digrafo", "matriz_adj");
 
     //------------------------------------ prim -----------------------------------------
 
