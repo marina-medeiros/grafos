49edec022940cecbfb161bf617579dd56e94f9e1
marina-medeiros <marinamaleite@gmail.com>
2025-12-04 08:27:49 -0300

    main com busca_local ok

--- unidade3/main.cpp
+++ unidade3/main.cpp
@@ -115,7 +115,9 @@ int main() {
   analisar_digrafo(digrafo_problema5, "problema_5", true);
   auto res_vizinho5 = vizinho_mais_proximo(digrafo_problema5, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho5, true);
+  imprimir_busca_local(res_vizinho5, digrafo_problema5); /////////////
   auto res_insercao5 = insercao_mais_barata(digrafo_problema5, 0);
+  imprimir_busca_local(res_insercao5, digrafo_problema5); /////////////
   imprimir_resultado("Inserção Mais Barata", res_insercao5, true);
 
   DigrafoMatrizAdj digrafo_problema6(0);
@@ -123,54 +125,68 @@ int main() {
   analisar_digrafo(digrafo_problema6, "problema_6");
   auto res_vizinho6 = vizinho_mais_proximo(digrafo_problema6, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho6, false);
+  imprimir_busca_local(res_vizinho6, digrafo_problema6); /////////////
   auto res_insercao6 = insercao_mais_barata(digrafo_problema6, 0);
   imprimir_resultado("Inserção Mais Barata", res_insercao6, false);
+  imprimir_busca_local(res_insercao6, digrafo_problema6); /////////////
 
   DigrafoMatrizAdj digrafo_problema7(0);
   digrafo_problema7.carregar_de_arquivo_csv("../dados/PROBLEMA_7.csv", true);
   analisar_digrafo(digrafo_problema7, "problema_7", true);
   auto res_vizinho7 = vizinho_mais_proximo(digrafo_problema7, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho7, true);
+  imprimir_busca_local(res_vizinho7, digrafo_problema7); /////////////
   auto res_insercao7 = insercao_mais_barata(digrafo_problema7, 0);
   imprimir_resultado("Inserção Mais Barata", res_insercao7, true);
+  imprimir_busca_local(res_insercao7, digrafo_problema7); /////////////
 
   DigrafoMatrizAdj digrafo_problema8(0);
   digrafo_problema8.carregar_de_arquivo_csv("../dados/PROBLEMA_8.csv");
   analisar_digrafo(digrafo_problema8, "problema_8");
   auto res_vizinho8 = vizinho_mais_proximo(digrafo_problema8, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho8, false);
+  imprimir_busca_local(res_vizinho8, digrafo_problema8); /////////////
   auto res_insercao8 = insercao_mais_barata(digrafo_problema8, 0);
   imprimir_resultado("Inserção Mais Barata", res_insercao8, false);
+  imprimir_busca_local(res_insercao8, digrafo_problema8); /////////////
 
   DigrafoMatrizAdj digrafo_problema9(0);
   digrafo_problema9.carregar_de_arquivo_csv("../dados/PROBLEMA_9.csv", true);
   analisar_digrafo(digrafo_problema9, "problema_9", true);
   auto res_vizinho9 = vizinho_mais_proximo(digrafo_problema9, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho9, true);
+  imprimir_busca_local(res_vizinho9, digrafo_problema9); /////////////
   auto res_insercao9 = insercao_mais_barata(digrafo_problema9, 0);
   imprimir_resultado("Inserção Mais Barata", res_insercao9, true);
+  imprimir_busca_local(res_insercao9, digrafo_problema9); /////////////
 
   DigrafoMatrizAdj digrafo_problema10(0);
   digrafo_problema10.carregar_de_arquivo_csv("../dados/PROBLEMA_10.csv");
   analisar_digrafo(digrafo_problema10, "problema_10");
   auto res_vizinho10 = vizinho_mais_proximo(digrafo_problema10, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho10, false);
+  imprimir_busca_local(res_vizinho10, digrafo_problema10); /////////////
   auto res_insercao10 = insercao_mais_barata(digrafo_problema10, 0);
   imprimir_resultado("Inserção Mais Barata", res_insercao10, false);
+  imprimir_busca_local(res_insercao10, digrafo_problema10); /////////////
 
   DigrafoMatrizAdj digrafo_problema11(0);
   digrafo_problema11.carregar_de_arquivo_csv("../dados/PROBLEMA_11.csv", true);
   analisar_digrafo(digrafo_problema11, "problema_11", true);
   auto res_vizinho11 = vizinho_mais_proximo(digrafo_problema11, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho11, true);
+  imprimir_busca_local(res_vizinho11, digrafo_problema11); /////////////
   auto res_insercao11 = insercao_mais_barata(digrafo_problema11, 0);
   imprimir_resultado("Inserção Mais Barata", res_insercao11, true);
+  imprimir_busca_local(res_insercao11, digrafo_problema11); /////////////
 
   DigrafoMatrizAdj digrafo_problema12(0);
   digrafo_problema12.carregar_de_arquivo_csv("../dados/PROBLEMA_12.csv");
   analisar_digrafo(digrafo_problema12, "problema_12");
   auto res_vizinho12 = vizinho_mais_proximo(digrafo_problema12, 0);
   imprimir_resultado("Vizinho Mais Próximo", res_vizinho12, false);
+  imprimir_busca_local(res_vizinho12, digrafo_problema12); /////////////
   auto res_insercao12 = insercao_mais_barata(digrafo_problema12, 0);
   imprimir_resultado("Inserção Mais Barata", res_insercao12, false);
+  imprimir_busca_local(res_insercao12, digrafo_problema12); /////////////
 }
