e46d4643a314b2eb80d57d72d53f444a0416cebc
raquellzz <raquelcfapoio24@gmail.com>
2025-12-04 23:00:33 -0300

    add: testes

--- unidade2/headers/hierholzer.h
+++ unidade2/headers/hierholzer.h
@@ -3,6 +3,7 @@
 
 #include "Grafo.h"
 #include "DigrafoListaAdj.h"
+#include "GrafoListaAdj.h"
 
 void hierholzer_grafo(const GrafoListaAdj& grafo);
 void hierholzer_digrafo(const DigrafoListaAdj& digrafo);
